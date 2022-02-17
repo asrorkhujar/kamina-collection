e923c93f5f036918534d33efaca435af7578491c
coder-1990 <asrorakmalovich@gmail.com>
2022-02-17 16:34:09 +0500

    add text styles

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 ;(function () {
-	
+
 	'use strict';
 
-	// iPad and iPod detection	
+	// iPad and iPod detection
 	var isiPad = function(){
 		return (navigator.platform.indexOf("iPad") != -1);
 	};
 
 	var isiPhone = function(){
 	    return (
-			(navigator.platform.indexOf("iPhone") != -1) || 
+			(navigator.platform.indexOf("iPhone") != -1) ||
 			(navigator.platform.indexOf("iPod") != -1)
 	    );
 	};
@@ -18,7 +18,7 @@
 
 	// Carousel Feature Slide
 	var testimonialCarousel = function(){
-		
+
 		var owl = $('.owl-carousel-fullwidth');
 		owl.owlCarousel({
 			animateOut: 'fadeOut',
@@ -33,7 +33,7 @@
 	};
 
 	var sliderMain = function() {
-		
+
 	  	$('#qbootstrap-slider-hero .flexslider').flexslider({
 			animation: "fade",
 			slideshowSpeed: 5000,
@@ -63,9 +63,9 @@
 		$('.animate-box').waypoint( function( direction ) {
 
 			if( direction === 'down' && !$(this).hasClass('animated') ) {
-			
+
 				$(this.element).addClass('fadeInUp animated');
-			
+
 			}
 
 		} , { offset: '75%' } );
@@ -79,13 +79,13 @@
 		$('body').on('click', '.js-qbootstrap-nav-toggle', function(event){
 
 			if ( $('#navbar').is(':visible') ) {
-				$(this).removeClass('active');	
+				$(this).removeClass('active');
 			} else {
-				$(this).addClass('active');	
+				$(this).addClass('active');
 			}
 
 			event.preventDefault();
-			
+
 		});
 
 	};
@@ -135,11 +135,11 @@
 	var navigationSection = function() {
 
 		var $section = $('div[data-section]');
-		
+
 		$section.waypoint(function(direction) {
 		  	if (direction === 'down') {
 		    	navActive($(this.element).data('section'));
-		    
+
 		  	}
 		}, {
 		  	offset: '150px'
@@ -174,8 +174,8 @@
 						header.removeClass('navbar-fixed-top qbootstrap-animated slideInDown slideOutUp');
 					}, 100 );
 				}
-			} 
-			
+			}
+
 		});
 	};
 
@@ -187,7 +187,7 @@
 		$('.animate-box').waypoint( function( direction ) {
 
 			if( direction === 'down' && !$(this.element).hasClass('animated') ) {
-				
+
 				i++;
 
 				$(this.element).addClass('item-animate');
@@ -210,9 +210,9 @@
 							el.removeClass('item-animate');
 						},  k * 50, 'easeInOutExpo' );
 					});
-					
+
 				}, 50);
-				
+
 			}
 
 		} , { offset: '85%' } );
@@ -249,7 +249,7 @@
 
 		});
 	};
-	
+
 
 	// Set the date we're counting down to
 		var countDownDate = new Date("Dec 02, 2017 15:37:25").getTime();
@@ -279,18 +279,18 @@
 		document.getElementById("minutes").innerHTML = minutes + " <small>minutes</small> ";
 		document.getElementById("seconds").innerHTML = seconds + " <small>seconds</small> ";
 
-		// If the count down is finished, write some text 
+		// If the count down is finished, write some text
 		if (distance < 0) {
 		 clearInterval(x);
 		 document.getElementById("demo").innerHTML = "The Wedding Ceremony is Over";
 		}
-		}, 1000);	
-	
-		
+		}, 1000);
+
+
 	var bgVideo = function() {
 		$('.player').mb_YTPlayer();
 	};
-        
+
 
 	// Document on load.
 	$(function(){
